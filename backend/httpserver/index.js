49b6a5340b8087ff8f6bcb0a3127c2f99f1d5e73
Thinkatron <amcanalley16@gmail.com>
2016-12-19 14:09:48 -0600

    Update index.js

--- backend/httpserver/index.js
+++ backend/httpserver/index.js
@@ -1,1 +1,27 @@
 
+var serveport       = 80,
+    gameport        = 3586,
+    express         = require('express'),
+    UUID            = require('node-uuid'),
+    verbose         = true,
+    https           = require('https'),
+    fs              = require('fs'),
+    exp             = express(),
+//    gameProcess     = require("./gameProcess.gyp"),
+    wsserver        = require('ws').Server;
+
+var httpsServer = https.createServer({
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem')
+}, exp).listen(serveport);
+if(verbose) console.log(" ")
+if(verbose) console.log('\t :: Express :: Opened Port Listener :: Port ' + serveport);
+
+exp.get('/', function( req, res ){ 
+    res.sendFile(__dirname + '/HTML/Index.html');
+    });
+exp.get( '/*' , function( req, res, next ) {
+    var file = req.params[0]; 
+    if(verbose) console.log('\t :: Express :: file requested :: ' + file);
+    res.sendFile( __dirname + '/HTML/' + file );
+    });
